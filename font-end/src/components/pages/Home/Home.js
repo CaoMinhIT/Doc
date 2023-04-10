886700bee655f05576b6656a5b7057f75541adf2
BabyShark <maingochan1709@gmail.com>
2023-04-11 01:16:06 +0700

    fix2

--- font-end/src/components/pages/Home/Home.js
+++ font-end/src/components/pages/Home/Home.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import classes from './Home.module.css';
 import NewsCard from './news/NewsCard';
 import HotNews from './news/HotNews';
 import Layout from '../../layout/Layout';
-import { dummyDataNews  } from '../../dummyData/dummyData';
+import { getNews } from '../../../api/newsApi';
 const Home = () => {
+    const [newsList, setNewsList] = useState([]);
+    useEffect(() => {
+        const token = localStorage.getItem('access-token');
+        const getNewsList = async () => {
+            try {
+                const res = await getNews(token);
+                setNewsList(res.data);
+                console.log("data", res.data);
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+        getNewsList();
+    }, [])
     return (
         <Layout>
             <img className={classes.banner} src="https://thumbs.dreamstime.com/b/orange-sky-over-sea-sunset-coloring-clouds-bright-light-51802926.jpg" alt="banner"></img>
             <h1 className={classes.color_pink}>Tin mới nhất</h1>
             <div className="row">
-                {dummyDataNews.slice(0,8).map((item, index) => {
+                {newsList.slice(0,8).map((item, index) => {
                     return (
                         <div key={index} className="col-3">
                             <NewsCard news={item} color="pink" />
@@ -19,7 +33,7 @@ const Home = () => {
                 })}
             </div>
             <h1 className={classes.color_blue}>Tin hot</h1>
-            <HotNews news={dummyDataNews} />
+            <HotNews news={newsList} />
         </Layout>
     )
 }
